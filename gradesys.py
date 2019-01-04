0f981872fe7e0d4d45bb44ee53a7d45505be45ab
Qaphantom <42305604+Qaphantom@users.noreply.github.com>
2019-01-04 16:31:54 -0600

    Create gradesys.py

---
+++ gradesys.py
@@ -0,0 +1,10 @@
+grade = 12 
+
+if grade > 100:
+	print("Not a valid grade") 
+elif grade < 0:
+	print("Not a Valid grade") 
+elif grade >= 76:
+	print("Good job, You Passed")
+elif grade < 76:
+	print("You Failed. Go work at Mc Donalds")
